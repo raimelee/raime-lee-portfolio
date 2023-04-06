c023e8c7826ade70f30c52c83c1c71c4d9536340
raimelee <106028986+raimelee@users.noreply.github.com>
2023-04-06 12:07:35 -0400

    navbar and about adjustments 4/6/23 NOON

--- src/pages/About.js
+++ src/pages/About.js
@@ -41,17 +41,18 @@ const About = () => {
           <h2>Raime Lee</h2>
           <div class="line-dark"></div>
           <h3>Full Stack Visual Creative</h3>
-          <p>
-            <em>I am Raime</em>, a full stack web developer with experience in
-            design, marketing, copywriting, and fine art. This spring in
-            response to world and personal events, I was looking for a way to
-            make a greater difference in the world with my existing skill set,
-            when coding found me through a passing conversation. Soon after this
-            introduction, I enrolled in a 14-week full-time web development
-            bootcamp and graduated in September of 2022. Code is my new medium,
-            and with it, I hope to paint new possibilities and inspiring
-            experiences that equitably advance humanity and positively impact
-            the contexts in which we live.
+          <p className="bio-text">
+            <span className="bio-emphasis">I am Raime</span>, a self-motivated,
+            developer and designer with experience in graphic and web design,
+            marketing, copywriting, and fine art. In response to world and
+            personal events in 2022, I was looking for a way to make a greater
+            difference with my existing skillset, when I enrolled in and
+            graduated from a 14-week full-time web development bootcamp. This
+            new medium of code, combined with my visual expertise,
+            determination, empathy, and strategic mindset, allows me to paint
+            new possibilities and inspiring, user-centered experiences that
+            equitably advance humanity and positively impact the contexts in
+            which we live.
           </p>
         </section>
       </div>
